546a2a128fdd4c8b3d141d8ec95e60573411999a
unknown <aiss@yintong.com.cn>
2017-12-04 17:44:35 +0800

    添加导航

---
+++ vue-project/src/components/quiList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="qui-list">
+      <span class="list-tips">{{ tipsText}}</span>
+      <qui-button @btnClickEvent="btnClickEvent" :msg=msg class="small"></qui-button>
+  </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+import quiButton from './quiButton'
+export default {
+    components:{
+        'qui-button':quiButton
+    },
+    props:{
+        msg:{
+            default:'下载3'
+        },
+        tipsText:{
+            default:'默认值'
+        }
+    },
+    methods:{
+        btnClickEvent(){
+            alert(11111);
+        }
+    }
+  
+}
+</script>
--- vue-project/src/pages/index.vue
+++ vue-project/src/pages/index.vue
@@ -1,8 +1,27 @@
 <template>
-  <div class="mod-module mod-parallel">
+  <!-- <div class="mod-module mod-parallel">
     <a v-for="(item,index) in items" :class="[commonClass,item.active?activeClass:'']" @click="navClickEvent(items,index)">
       <span>{{ item.text}}</span>
     </a>
+  </div> -->
+    <div class="mod-module mod-parallel">
+    <div class="img-list type-full">
+      <div class="img-box">
+        <p class="img-item">
+          <a class="page-link" href="#/btn">按钮</a>
+        </p>
+      </div>
+      <div class="img-box">
+        <p class="img-item">
+          <a class="page-link" href="#/list">列表</a>
+        </p>
+      </div>
+      <div class="img-box">
+        <p class="img-item">
+          <a class="page-link" href="#/nav">导航</a>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,23 +42,23 @@ a{
     export default{
       data(){
         return{
-          commonClass:'nav-item',
-          activeClass:'active',
-            items:[
-                {
-                  text:'首页',
-                  active:true
-                },{
-                  text:'列表',
-                  active:false
-                },{
-                  text:'关于',
-                  active:false
-                },{
-                  text:'招聘',
-                  active:false
-                }
-            ]
+          // commonClass:'nav-item',
+          // activeClass:'active',
+          //   items:[
+          //       {
+          //         text:'首页',
+          //         active:true,
+          //       },{
+          //         text:'列表',
+          //         active:false
+          //       },{
+          //         text:'关于',
+          //         active:false
+          //       },{
+          //         text:'招聘',
+          //         active:false
+          //       }
+          //   ]
         }
       },
       methods:{
