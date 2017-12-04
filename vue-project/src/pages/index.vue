0396cd63d0228a61f2927a5af0e8f2e3e6afdd97
unknown <aiss@yintong.com.cn>
2017-12-04 16:54:13 +0800

    添加tab切换

--- vue-project/src/pages/index.vue
+++ vue-project/src/pages/index.vue
@@ -1,29 +1,55 @@
 <template>
   <div class="mod-module mod-parallel">
-    <div class="img-list type-full">
-      <div class="img-box">
-        <p class="img-item">
-          <a class="page-link" href="#/btn">按钮</a>
-        </p>
-      </div>
-      <div class="img-box">
-        <p class="img-item">
-          <a class="page-link" href="#/list">列表</a>
-        </p>
-      </div>
-      <div class="img-box">
-        <p class="img-item">
-          <a class="page-link" href="#/nav">导航</a>
-        </p>
-      </div>
-    </div>
+    <a v-for="(item,index) in items" :class="[commonClass,item.active?activeClass:'']" @click="navClickEvent(items,index)">
+      <span>{{ item.text}}</span>
+    </a>
   </div>
 </template>
-<style scoped>
 
+<style scoped>
+a{
+  display:inline-block;
+  height: 30px;
+  line-height: 30px;
+  width: 70px;
+  text-align: center;
+}
+.active{
+  background: #ccc;
+}
 </style>
 
 <script>
     export default{
+      data(){
+        return{
+          commonClass:'nav-item',
+          activeClass:'active',
+            items:[
+                {
+                  text:'首页',
+                  active:true
+                },{
+                  text:'列表',
+                  active:false
+                },{
+                  text:'关于',
+                  active:false
+                },{
+                  text:'招聘',
+                  active:false
+                }
+            ]
+        }
+      },
+      methods:{
+        navClickEvent(items,index){
+          items.forEach(function(item){
+            item.active = false;
+          });
+          items[index].active = true;
+          this.$emit('navClickEvent',items,index);
+        }
+      }
     }
 </script>
