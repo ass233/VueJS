86e2056c57e358f1147ff86dcffb3b06dc8f65a8
unknown <aiss@yintong.com.cn>
2017-12-07 22:11:48 +0800

    vue系统

---
+++ my-vue/src/base/scroll.vue
@@ -0,0 +1,14 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default{
+    
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
